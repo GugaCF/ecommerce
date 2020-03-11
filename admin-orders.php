c632c1d1da812e04cd39d51b2622a0789a7b4ac6
Gustavo Campos Freitas <gugacf@yahoo.com.br>
2020-03-11 12:24:40 -0300

    Admin - Pedidos

--- admin-orders.php
+++ admin-orders.php
@@ -24,6 +24,36 @@ $app->get("/admin/orders/:idorder/status", function($idorder){
 
 });
 
+$app->post("/admin/orders/:idorder/status", function($idorder){
+
+	User::verifyLogin();
+
+	if (!isset($_POST['idstatus']) || !(int)$_POST['idstatus'] > 0) {
+
+		Order::setError("Informe o status atual.");
+
+		header("Location: /admin/orders/".$idorder."/status");
+
+		exit;
+
+	}
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$order->setidstatus((int)$_POST['idstatus']);
+
+	$order->save();
+
+	Order::setSuccess("Status atualizado.");
+
+	header("Location: /admin/orders/".$idorder."/status");
+
+	exit;
+
+});
+
 $app->get("/admin/orders/:idorder/delete", function($idorder){
 
 	User::verifyLogin();
